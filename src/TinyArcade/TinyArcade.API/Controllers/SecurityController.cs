e684b484975678e19b9594bfff7edb734036b77d
beunprogrammeur <12196127+beunprogrammeur@users.noreply.github.com>
2025-09-05 14:49:13 +0100

    added the TinyArcade solution with skeleton API project

---
+++ src/TinyArcade/TinyArcade.API/Controllers/SecurityController.cs
@@ -0,0 +1,12 @@
+﻿namespace TinyArcade.API.Controllers
+{
+    public class SecurityController
+    {
+        // TODO: add endpoints:
+
+        // register
+        // log in
+        // change password
+        // make admin (only if you're already an admin)
+    }
+}
